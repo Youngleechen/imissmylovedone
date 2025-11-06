3dd7616f2c99e01ef6b6da50be0925fbdcb421c1
Youngleechen <137005469+Youngleechen@users.noreply.github.com>
2025-11-06 09:12:48 +0300

    Update script.js

--- homepage/script.js
+++ homepage/script.js
@@ -1,3 +1,7 @@
-document.getElementById('loginButton').addEventListener('click', function () {
-  window.location.href = 'https://imissmylovedone.vercel.app/test';
+// Get the button element
+const purpleButton = document.getElementById('purpleButton');
+
+// Add click event listener to navigate to the test page
+purpleButton.addEventListener('click', function() {
+    window.location.href = 'https://imissmylovedone.vercel.app/test';
 });
