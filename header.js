565b6226fa7a79feb66fd04ab9277850c6770874
Youngleechen <137005469+Youngleechen@users.noreply.github.com>
2025-11-12 09:55:51 +0300

    Create header.js

---
+++ header.js
@@ -0,0 +1,13 @@
+// header.js
+export function Header() {
+  return `
+    <header class="header">
+      <div class="logo">
+        <i class="fas fa-heart-circle"></i> Healing
+      </div>
+      <div class="user-menu">
+        <div class="user-avatar">U</div>
+      </div>
+    </header>
+  `;
+}
